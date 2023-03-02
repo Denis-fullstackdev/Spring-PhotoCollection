ec05480f98e7045b403f638ddd61f158c28992da
Denis-92 <d.mozenic@gmail.com>
2023-03-02 09:50:37 +0100

    init project

---
+++ src/main/java/com/corsojava/springboot/SpringMioFotoalbumApplication.java
@@ -0,0 +1,13 @@
+package com.corsojava.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMioFotoalbumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMioFotoalbumApplication.class, args);
+	}
+
+}
